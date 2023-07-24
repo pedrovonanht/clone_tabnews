e46eb517df9faac0d206b67bcf8a14bd463ce780
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2023-07-24 17:40:49 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>"An Emperor, sovering of dominions vast, not by the crown he wears, but by the steadfatness of his spirit and the magnimity of his heart"</h1>
+}
+
+export default Home;
