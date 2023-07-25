9cd18676a1af448b2f866d36e8309f76b09ab25b
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2023-07-25 16:46:03 +0000

    adiciona o conteudo da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home(){
-    return <h1>"An Emperor, sovering of dominions vast, not by the crown he wears, but by the steadfatness of his spirit and the magnimity of his heart"</h1>
+    return (
+        <div>
+            <h1>Eu estou aprendendo a criar sites no curso.dev</h1>
+            <p>esse é um dos desafios do curso, então se gostou responde com um joinha a mensagem e da uma olhada nesse <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&t">vídeo</a></p>
+            <br></br>
+            
+        </div>
+      )
 }
+
 
 export default Home;
