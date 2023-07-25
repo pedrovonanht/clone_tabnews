8c8e87b3da09af7c7ac61b8b6299ed668b5342ed
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2023-07-25 17:12:50 +0000

    removido arquivo de teste index.html

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ function Home(){
     return (
         <div>
             <h1>Eu estou aprendendo a criar sites no curso.dev</h1>
-            <p>esse é um dos desafios do curso, então se gostou responde com um joinha a mensagem e da uma olhada nesse <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&t">vídeo</a></p>
-            <br></br>
+            <p>esse é um dos desafios do curso, então se gostou responde com um joinha a mensagem 
+                e da uma olhada nesse <a href="https://www.youtube.com/watch?time_continue=1&v=mx86-rTclzA&embeds_
+                referring_euri=https%3A%2F%2Fwww.google.com%2F&source_ve_path=MjM4NTE&feature=emb_title">vídeo</a></p>
+            <br></br>git
             
         </div>
       )
