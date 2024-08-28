300e740e73e38d2d3ded74427a13131a8147f15d
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2024-08-28 20:28:27 +0000

    tests git actions worflow running

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -2,8 +2,8 @@ import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await database.query("drop schema public cascade; create schema public;");
   await orchestrator.waitForAllServices()
+  await database.query("drop schema public cascade; create schema public;");
 });
 
 test("POST to /api/v1/migrations should return 200", async () => {
