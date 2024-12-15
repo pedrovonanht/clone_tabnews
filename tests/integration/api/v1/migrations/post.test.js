2726ad25ee13c6272b87a460c414c26017d77572
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2024-12-15 01:00:02 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -5,24 +5,36 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
 });
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("for the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+        expect(response1.status).toBe(201); //espero um response created status
+
+        const responseBody1 = await response1.json();
+
+        expect(Array.isArray(responseBody1)).toBe(true); //espero que seja um array
+        expect(responseBody1.length).toBeGreaterThan(0); //espero que ele não venha vazio
+      });
+      test("for the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response2.status).toBe(200); //espero que a conexão seja bem sucedida e que nada seja criado
+        const responseBody2 = await response2.json();
+        expect(responseBody2.length).toBe(0); //espero que o array venha vazio já que a migrations já foi rodada no live-run
+      });
+    });
   });
-
-  expect(response1.status).toBe(201); //espero um response created status
-
-  const responseBody1 = await response1.json();
-
-  expect(Array.isArray(responseBody1)).toBe(true); //espero que seja um array
-  expect(responseBody1.length).toBeGreaterThan(0); //espero que ele não venha vazio
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  expect(response2.status).toBe(200); //espero que a conexão seja bem sucedida e que nada seja criado
-  const responseBody2 = await response2.json();
-  expect(responseBody2.length).toBe(0); //espero que o array venha vazio já que a migrations já foi rodada no live-run
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,23 +3,26 @@ import orchestrator from "tests/orchestrator.js";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("retriving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+      //verify updated_at time
+      const responseBody = await response.json();
 
-  //verify updated_at time
-  const responseBody = await response.json();
+      const ParsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); //to garantee that the value is a date
 
-  const ParsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); //to garantee that the value is a date
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.updated_at).toEqual(ParsedUpdatedAt); // to protect from a null object
 
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.updated_at).toEqual(ParsedUpdatedAt); // to protect from a null object
+      expect(responseBody.dependencies.database.used_connections).toEqual(1); //em ambiente de produção já que só há um desenvolvedor o esperado é só 1 conexão
 
-  expect(responseBody.dependencies.database.used_connections).toEqual(1); //em ambiente de produção já que só há um desenvolvedor o esperado é só 1 conexão
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.version).toBeDefined();
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.version).toBeDefined();
+    });
+  });
 });
