c820e810117e5a8689b3422a5ca98bbd189762bf
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-04-13 23:20:05 +0000

    feat: creates 'api/v1/sessions'

--- models/user.js
+++ models/user.js
@@ -91,6 +91,37 @@ async function findOneByUsername(username) {
   }
 }
 
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const results = await database.query({
+      text: `
+      SELECT
+      *
+      FROM
+      users
+      WHERE
+      LOWER(email) = LOWER($1)
+      LIMIT
+      1
+      ;`,
+      values: [email],
+    });
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O email informado não foi encontrado no sistema",
+        action: "verifique o email informado.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
+
 async function validateUniqueEmail(email) {
   const results = await database.query({
     text: `
@@ -167,6 +198,7 @@ const user = {
   create,
   findOneByUsername,
   update,
+  findOneByEmail
 };
 
 export default user;
