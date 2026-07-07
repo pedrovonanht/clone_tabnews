e81e8c8b9895cbda459affa242fa29d945959048
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-07-07 20:52:39 +0000

    feat: implements `DELETE` method to `api/v1/sessions`

--- models/session.js
+++ models/session.js
@@ -83,10 +83,36 @@ async function renew(sessionId) {
     return results.rows[0];
   }
 }
+
+async function expireById(sessionId) {
+  const expiredSessionObject = await runUpdateQuery(sessionId)
+  return expiredSessionObject;
+
+  async function runUpdateQuery(sessionId) {
+    const results = await database.query({
+      text: `
+      UPDATE
+        sessions
+      SET
+        expires_at = expires_at - interval '1 year',
+        updated_at = now()
+      WHERE 
+        id=$1
+      RETURNING
+        * 
+      ;
+      `,
+      values: [sessionId],
+    });
+
+    return results.rows[0];
+  }
+}
 const session = {
   create,
   findOneValidByToken,
   renew,
+  expireById,
   EXPIRATIONS_IN_MILLISECONDS,
 };
 
