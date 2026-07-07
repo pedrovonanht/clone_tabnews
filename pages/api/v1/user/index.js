d2f2a4e165442538155af7af43b7389b9e01f7d2
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-07-07 19:42:33 +0000

    style: run prettier

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -18,6 +18,9 @@ async function getHandler(request, response) {
 
   controller.setSessionCookie(renewedSessionObject.token, response);
 
-  response.setHeader("Cache-Control", "no-store, no-cache, max-age=0, must-revalidate")
+  response.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, max-age=0, must-revalidate",
+  );
   return response.status(200).json(userFound);
 }
